e3b56a11705ee17569fab36adcfe04ff4a308433
Stanislav <yanut995@gmail.com>
2025-03-24 14:58:13 +0300

    Чуток починил регистрацию, иду дальше

--- src/components/Global/Auth/Registration/AuthRegForm/AuthRegForm.jsx
+++ src/components/Global/Auth/Registration/AuthRegForm/AuthRegForm.jsx
@@ -1,3 +1,4 @@
+// File: /src/components/Global/Auth/Registration/AuthRegForm/AuthRegForm.jsx
 import React from "react";
 import { Steps, Button } from "antd";
 import { useNavigate } from "react-router-dom";
@@ -11,9 +12,8 @@ import { LockTwoTone, MailTwoTone, PhoneTwoTone } from "@ant-design/icons";
 const { Step } = Steps;
 
 const AuthRegForm = () => {
-  const { registrationStep } = useRegistration((state) => ({
-    registrationStep: state.registrationStep,
-  }));
+  // Получаем registrationStep напрямую из Zustand
+  const registrationStep = useRegistration((state) => state.registrationStep);
   const navigate = useNavigate();
 
   const handleClick = () => {
@@ -22,11 +22,7 @@ const AuthRegForm = () => {
 
   return (
     <div>
-      <Steps
-        current={registrationStep}
-        className={styles.steps}
-        direction="vertical"
-      >
+      <Steps current={registrationStep} className={styles.steps} direction="vertical">
         <Step
           title="Номер мобильного телефона"
           icon={<PhoneTwoTone />}
@@ -35,23 +31,15 @@ const AuthRegForm = () => {
         <Step
           title="Электронная почта"
           icon={<MailTwoTone />}
-          status={
-            registrationStep === 1
-              ? "process"
-              : registrationStep <= 1
-              ? "wait"
-              : "finish"
-          }
+          status={registrationStep === 1 ? "process" : registrationStep <= 1 ? "wait" : "finish"}
         />
-        <Step
-          title="Пароль"
-          icon={<LockTwoTone />}
-          status={registrationStep === 2 ? "process" : "wait"}
-        />
+        <Step title="Пароль" icon={<LockTwoTone />} status={registrationStep === 2 ? "process" : "wait"} />
       </Steps>
+
       {registrationStep === 0 && <PhoneVerification />}
       {registrationStep === 1 && <EmailVerification />}
       {registrationStep === 2 && <PasswordRegForm />}
+
       <Button type="link" onClick={handleClick}>
         Инструкция по регистрации
       </Button>
