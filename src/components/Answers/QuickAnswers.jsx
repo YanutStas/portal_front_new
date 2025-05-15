7847e7b19fb4ec5bd7094a42284c093cc9ba3fdb
luxeivan <luxeivan@gmail.com>
2025-05-15 11:36:08 +0300

    Поменял Энергетика

--- src/components/Answers/QuickAnswers.jsx
+++ src/components/Answers/QuickAnswers.jsx
@@ -13,8 +13,10 @@ import ModalBot from "../Global/ModalBot";
 import useGlobal from "../../stores/useGlobal";
 import axios from "axios";
 import MarkDownText from "../MarkDownText/MarkDownText";
-import mosoblikImage from "../../img/answers/mosoblik.png";
-import mosoblikDarkImage from "../../img/answers/mosoblik_dark.png";
+import mosoblikImage from "../../img/answers/energetic1.png";
+import mosoblikDarkImage from "../../img/answers/energetic1_dark.png";
+// import mosoblikImage from "../../img/answers/mosoblik.png";
+// import mosoblikDarkImage from "../../img/answers/mosoblik_dark.png";
 import styles from "./QuickAnswers.module.css";
 
 const backServer = import.meta.env.VITE_BACK_BACK_SERVER;
@@ -85,7 +87,7 @@ export default function QuickAnswers() {
             <Button
               type="primary"
               onClick={() => setChatModalVisible(true)}
-              style={{ marginTop: "20px" }}
+              style={darkMode && { border: "0px", boxShadow: "0px 0px 60px 60px rgba(255, 255, 255, 0.2),  0px 0px 40px 40px rgba(255, 255, 255, 0.5),  0px 0px 20px 20px rgba(255, 255, 255, 0.6),  inset 0px 0px 5px 2px rgba(255, 255, 255, 0.6)" }}
               size="large"
               className={styles.btn}
             >
@@ -99,6 +101,6 @@ export default function QuickAnswers() {
         visible={chatModalVisible}
         onClose={() => setChatModalVisible(false)}
       />
-    </div>
+    </div >
   );
 }
