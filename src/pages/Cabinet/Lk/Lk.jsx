c1c20b8131ba84f6672a2eb5e1d6861a71641907
luxeivan <luxeivan@gmail.com>
2025-06-27 09:58:51 +0300

    Поправил

--- src/pages/Cabinet/Lk/Lk.jsx
+++ src/pages/Cabinet/Lk/Lk.jsx
@@ -3,11 +3,12 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import CardClaim from '../Claims/CardClaim';
 import usePersonalAccounts from '../../../stores/Cabinet/usePersonalAccount';
+import Preloader from '../../../components/Main/Preloader'
 
 export default function Lk() {
     const [openDrawer, setOpenDrawer] = useState(false)
     // const [lk, setLk] = useState(null)
-    const [listClaims, setListClaims] = useState(null)
+    // const [listClaims, setListClaims] = useState(null)
     const [listCompletedClaims, setListCompletedClaims] = useState(null)
     const { token } = theme.useToken();
     const {
@@ -23,24 +24,6 @@ export default function Lk() {
     useEffect(() => {
         fetchPersonalAccountItem(id)
         fetchClaimsByPersonalAccount(id)
-        setListClaims([
-            {
-                Ref_Key: "c416e302-98df-11ef-9501-5ef3fcb042f8",
-                number: "000000110",
-                date: "2024-11-02T09:00:32",
-                service: {
-                    description: "Описание услуги"
-                }
-            },
-            {
-                Ref_Key: "c416e302-98df-11ef-9501-5ef3fcb042f9",
-                number: "000000111",
-                date: "2024-12-12T10:56:32",
-                service: {
-                    description: "Описание услуги №2"
-                }
-            }
-        ])
         setListCompletedClaims([
             {
                 Ref_Key: "c416e302-98df-11ef-9501-5ef3fcb042f7",
@@ -69,48 +52,57 @@ export default function Lk() {
     ]
     // console.log(token)
     return (
-        <div>
-            <Typography.Title style={{ margin: 0 }}>{personalAccount?.name}</Typography.Title>
-            <Flex gap={10} style={{ marginBottom: 10 }}>
+        <>
+            {loadingPersonalAccount && <Preloader />}
+            {!loadingPersonalAccount &&
+                <div>
+                    <Typography.Title style={{ margin: 0 }}>{personalAccount?.name}</Typography.Title>
+                    <Flex gap={10} style={{ marginBottom: 10 }}>
+                        {personalAccount?.inn &&
+                            <Typography.Text style={{ color: token.colorTextDescription }}>ИНН:{personalAccount?.inn}</Typography.Text>
+                        }
+                        {personalAccount?.kpp &&
+                            <Typography.Text style={{ color: token.colorTextDescription }}>КПП:{personalAccount?.kpp}</Typography.Text>
+                        }
+                    </Flex>
 
-                {personalAccount?.inn &&
-                    <Typography.Text style={{ color: token.colorTextDescription }}>ИНН:{personalAccount?.inn}</Typography.Text>
-                }
-                {personalAccount?.kpp &&
-                    <Typography.Text style={{ color: token.colorTextDescription }}>КПП:{personalAccount?.kpp}</Typography.Text>
-                }
-            </Flex>
+                    <Button onClick={() => { setOpenDrawer(true) }}>Подробнее о компании</Button>
+                    {loadingClaimsByPersonalAccount && <Preloader />}
+                    {!loadingClaimsByPersonalAccount &&
+                        <>
+                            <Divider orientation='left'>В работе</Divider>
+                            <Flex wrap={"wrap"} gap={20} style={{ marginTop: 20, marginBottom: 20 }}>
+                                {claimsByPersonalAccount && claimsByPersonalAccount.map((item, index) =>
+                                    <CardClaim item={item} key={index} />)}
+                            </Flex>
+                        </>
+                    }
+                    {/* <Collapse items={collap} /> */}
 
-            <Button onClick={() => { setOpenDrawer(true) }}>Подробнее о компании</Button>
-            <Divider orientation='left'>В работе</Divider>
-            <Flex wrap={"wrap"} gap={20} style={{ marginTop: 20, marginBottom: 20 }}>
-                {claimsByPersonalAccount && claimsByPersonalAccount.map((item, index) =>
-                    <CardClaim item={item} key={index} />)}
-            </Flex>
-            <Collapse items={collap} />
+                    <Drawer
+                        title={personalAccount?.name}
+                        open={openDrawer}
+                        onClose={() => { setOpenDrawer(false) }}
+                    >
+                        <Typography.Paragraph>
+                            Всего заявок в личном кабинете: {personalAccount?.totalClaims}
+                        </Typography.Paragraph>
+                        <Typography.Paragraph>
+                            Пользователи, которые имеют доступ к текущему личному кабинету:
+                        </Typography.Paragraph>
+                        <List
+                            bordered
+                            dataSource={personalAccount?.profiles?.map(item => item.email)}
+                            renderItem={item => (
+                                <List.Item>
+                                    <Typography.Text>{item}</Typography.Text>
+                                </List.Item>
+                            )}
+                        />
 
-            <Drawer
-                title={personalAccount?.name}
-                open={openDrawer}
-                onClose={() => { setOpenDrawer(false) }}
-            >
-                <Typography.Paragraph>
-                    Всего заявок в личном кабинете: {personalAccount?.totalClaims}
-                </Typography.Paragraph>
-                <Typography.Paragraph>
-                    Пользователи, которые имеют доступ к текущему личному кабинету:
-                </Typography.Paragraph>
-                <List
-                    bordered
-                    dataSource={personalAccount?.profiles?.map(item => item.email)}
-                    renderItem={item => (
-                        <List.Item>
-                            <Typography.Text>{item}</Typography.Text>
-                        </List.Item>
-                    )}
-                />
-
-            </Drawer>
-        </div>
+                    </Drawer>
+                </div>
+            }
+        </>
     )
 }
