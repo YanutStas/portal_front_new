718b7d160aec5ec573b92f81fd430081c3d574e8
luxeivan <luxeivan@gmail.com>
2025-07-09 13:20:07 +0300

    overflovHidden

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -147,7 +147,8 @@ export default function Services() {
                     >
                       <Card
                         style={{
-                          height: "100%"
+                          height: "100%",
+                          overflow:"hidden"
                         }}
                         classNames={{
                           body: `${styles.bodyCard}`
