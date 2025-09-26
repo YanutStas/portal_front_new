11284a81f44069cd89727481583858ba438d91f5
luxeivan <luxeivan@gmail.com>
2025-09-26 09:58:09 +0300

    Добавил дату файлам

--- src/components/FileForDownload.jsx
+++ src/components/FileForDownload.jsx
@@ -10,6 +10,7 @@ import png from "../img/docs/png.svg";
 import noext from "../img/docs/noext.svg";
 import styles from "./FileForDownload.module.css";
 import openDocs from "./Cabinet/openDocument";
+import moment from "moment";
 const typeFile = {
   pdf,
   doc,
@@ -22,9 +23,9 @@ const typeFile = {
   noext
 };
 const backServer = import.meta.env.VITE_BACK_BACK_SERVER;
-export default function FileForDownload({ type, id, name, size }) {
+export default function FileForDownload({ type, id, name, size, date = false }) {
   // console.log(url);
-  
+
   return (
     <a
       className={styles.docLine}
@@ -32,20 +33,23 @@ export default function FileForDownload({ type, id, name, size }) {
       // download=""
       // rel="noopener noreferrer"
       // target="_blank"
-      onClick={()=>{
+      onClick={() => {
         openDocs(id)
       }}
     >
       <div className={styles.docLine__wrapIcon}>
-        <img src={typeFile[type]?typeFile[type]:typeFile.noext} alt={`icon ${type}`} />
+        <img src={typeFile[type] ? typeFile[type] : typeFile.noext} alt={`icon ${type}`} />
       </div>
       <div className="docLine__wrapText">
         <span className={styles.docLine__name}>{name}</span>
         <span className={styles.docLine__fileInfo}>
-          {Number(size/1000) > 1000
-            ? `${(Number(size/1000) / 1000).toFixed(2)} МБ`
-            : `${Math.round(Number(size/1000))} КБ`}
+          {Number(size / 1000) > 1000
+            ? `${(Number(size / 1000) / 1000).toFixed(2)}МБ`
+            : `${Math.round(Number(size / 1000))}КБ`}
         </span>
+        {date &&
+          <span className={styles.docLine__fileInfo} style={{ marginLeft: 10 }}>{moment(date).format('DD.MM.YYYY HH.mm')}</span>
+        }
       </div>
     </a>
   );
