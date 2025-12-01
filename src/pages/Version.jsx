882a5926494905092e6d2b14fd276191ca03d8af
luxeivan <luxeivan@gmail.com>
2025-12-01 11:45:36 +0300

    Поправил отображение версий

--- src/pages/Version.jsx
+++ src/pages/Version.jsx
@@ -1,4 +1,4 @@
-import { Descriptions, Flex, List, Tabs, Typography } from 'antd';
+import { Card, Descriptions, Flex, List, Tabs, Typography, theme } from 'antd';
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import moment from 'moment';
@@ -6,6 +6,7 @@ import front from '../version.json'
 const backServer = import.meta.env.VITE_BACK_BACK_SERVER;
 
 export default function Version() {
+    const token = theme.useToken().token
     const [version, setVersion] = useState(false);
     useEffect(() => {
         const fetchVersion = async () => {
@@ -19,6 +20,7 @@ export default function Version() {
         fetchVersion();
     }, []);
 
+    // console.log(token);
 
     return (
         <div>
@@ -26,68 +28,65 @@ export default function Version() {
                 key: "1",
                 label: "Backend",
                 children: version &&
-                    <>
-                        <Typography.Title>Текущая версия: {version.data.current?.number}</Typography.Title>
-                        <Typography.Paragraph>{version.data.current?.comment}</Typography.Paragraph>
-                        <Typography.Title level={5}>Предыдущие версии</Typography.Title>
-                        <List
-                            // header={<div>Header</div>}
-                            // footer={<div>Footer</div>}
-                            bordered
-                            dataSource={version.data.versions.slice(1, version.data.versions.length)}
-                            renderItem={item => (
-                                <List.Item>
-                                    <Flex vertical gap={10}>
-                                        <Descriptions items={[
-                                            {
-                                                key: "1",
-                                                label: "Номер",
-                                                children: item.number,
-                                            },
-                                            {
-                                                key: "2",
-                                                label: "Дата",
-                                                children: moment(item.date).format('DD.MM.YYYY HH:mm'),
-                                            },
-                                        ]} />
-                                        <Typography.Paragraph>{item.comment}</Typography.Paragraph>
-                                    </Flex>
-                                </List.Item>
-                            )}
-                        />
-                    </>
-            },
-            {
-                key: "2",
-                label: "Frontend",
-                children: <>
-                    <Typography.Title>Текущая версия: {front[0]?.version}</Typography.Title>
-                    <Typography.Paragraph>{front[0]?.comment}</Typography.Paragraph>
-                    <Typography.Title level={5}>Предыдущие версии</Typography.Title>
-                    <List
-                        bordered
-                        dataSource={front.slice(1, front.length)}
-                        renderItem={item => (
-                            <List.Item>
-                                <Flex vertical gap={10}>
-                                    <Descriptions items={[
+                    <Flex vertical gap={5}>
+                        {version.data.versions.map((item, index) => (
+                            <Card style={{ backgroundColor: index === 0 ? token.colorPrimaryBg : undefined }}>
+                                <Descriptions
+                                    style={{ marginBottom: 10 }}
+                                    items={[
                                         {
                                             key: "1",
                                             label: "Номер",
-                                            children: item.version,
+                                            children: item.number,
                                         },
                                         {
                                             key: "2",
                                             label: "Дата",
-                                            children: item.date,
+                                            children: moment(item.date).format('DD.MM.YYYY HH:mm'),
                                         },
-                                    ]} />
+
+                                    ]}
+
+
+                                />
+                                <Typography.Paragraph>{item.comment}</Typography.Paragraph>
+                            </Card>
+
+                        ))}
+                    </Flex>
+
+
+            },
+            {
+                key: "2",
+                label: "Frontend",
+                children:                 <Flex vertical gap={5}>
+                            {front.map((item, index) => (
+                                <Card style={{ backgroundColor: index === 0 ? token.colorPrimaryBg : undefined }}>
+                                    <Descriptions
+                                    style={{marginBottom:10}}
+                                        items={[
+                                            {
+                                                key: "1",
+                                                label: "Номер",
+                                                children: item.version,
+                                            },
+                                            {
+                                                key: "2",
+                                                label: "Дата",
+                                                children: item.date,
+                                            },
+
+                                        ]}
+
+
+                                    />
                                     <Typography.Paragraph>{item.comment}</Typography.Paragraph>
-                                </Flex>
-                            </List.Item>
-                        )}
-                    />
-                </>
+                                </Card>
+
+                            ))}
+                        </Flex>
+                
             }
             ]} />
 
