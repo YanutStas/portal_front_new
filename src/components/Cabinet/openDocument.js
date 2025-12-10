60b82c3326ee03cc1a1bd59a6b1c8f8fa239dadd
luxeivan <luxeivan@gmail.com>
2025-12-10 09:40:46 +0300

    поправил проверку SIG

--- src/components/Cabinet/openDocument.js
+++ src/components/Cabinet/openDocument.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import b64toBlob from "../../lib/b64toBlob";
 
-export default async function openDocs(documentId, sig = false) {
+export default async function openDocs(documentId, filename = "document.pdf", download = false, sig = false,) {
     const backServer = import.meta.env.VITE_BACK_BACK_SERVER;
     try {
         const res = await axios
@@ -11,17 +11,26 @@ export default async function openDocs(documentId, sig = false) {
                 },
                 withCredentials: true,
             })
-            console.log(res.data);
-            
+        // console.log(res.data);
+
         if (res.data?.data?.base64) {
             const file = b64toBlob(res.data.data.base64, sig ? "application/pgp-signature" : "application/pdf")
             const fileURL = URL.createObjectURL(file);
-            if (sig) {
-                const newWindow = window.open(fileURL);
-            } else {
-                const newWindow = window.open("", "_blank");
-                newWindow.location.href = fileURL;
-            }
+            // if (sig) {
+            //     const newWindow = window.open(fileURL);
+            // } else {
+            //     const newWindow = window.open("", "_blank");
+            //     newWindow.location.href = fileURL;
+            // }
+            const link = document.createElement('a');
+            link.href = fileURL;
+            download ? link.download = filename : link.target = "_blank";
+            
+            document.body.appendChild(link);
+            link.click();
+
+            document.body.removeChild(link);
+            window.URL.revokeObjectURL(fileURL);
             return true
         } else {
             return false
--- src/components/FileForDownload.jsx
+++ src/components/FileForDownload.jsx
@@ -13,8 +13,9 @@ import noext from "../img/docs/noext.svg";
 import styles from "./FileForDownload.module.css";
 import openDocs from "./Cabinet/openDocument";
 import moment from "moment";
-import { Button, Descriptions, Flex, message, Modal, Typography } from "antd";
+import { Button, Descriptions, Flex, message, Modal, Spin, Typography, theme, Tooltip } from "antd";
 import checkSig from "./Cabinet/checkSig";
+import { DownloadOutlined, SafetyOutlined } from '@ant-design/icons'
 const typeFile = {
   pdf,
   doc,
@@ -29,123 +30,181 @@ const typeFile = {
 };
 // const backServer = import.meta.env.VITE_BACK_BACK_SERVER;
 export default function FileForDownload({ type, id, name, size, date = false, signs = false, sig = false, idDocForCheckSig = false }) {
+  const token = theme.useToken().token
   const [chekingValue, setChekingValue] = useState(false)
+  const [downloading, setDownloading] = useState(false)
   const [isChecking, setIsChecking] = useState(false)
-  // console.log(url);
+  // console.log(token);
   const [messageApi, contextHolder] = message.useMessage();
 
   return (
     <>
-      <Flex vertical>
+      <Flex vertical gap={10} style={{ marginBottom: 20, marginTop: 20 }}>
         {contextHolder}
-        <Flex style={{ marginBottom: 10 }} gap={5} vertical>
+        <Flex gap={5} align="center" >
+          <Flex gap={10} align="center">
+            <a
+              onClick={async () => {
+                if (!downloading) {
+                  setDownloading(true)
+                  try {
+                    if (!await openDocs(id, name, false, sig)) {
+                      messageApi.open({
+                        type: 'error',
+                        content: 'Ошибка скачивания файла',
+                      });
+                    }
+                  } catch (error) {
+                    console.log(error);
 
-          <a
-            // className={styles.docLine}
-            // href={`${backServer}/uploads/${url}`}
-            // download=""
-            // rel="noopener noreferrer"
-            // target="_blank"
+                  }
+                  setDownloading(false)
+                }
+              }}
+            >
+              <Flex align="center" gap={5} >
+
+                <Flex justify="center" align="center" >
+                  <img src={typeFile[type] ? typeFile[type] : typeFile.noext} alt={`icon ${type}`} style={{ maxWidth: 50 }} />
+                </Flex>
+                <Flex vertical>
+                  <span className={styles.docLine__name} style={{ color: token.colorText }}>{name}</span>
+                  <span className={styles.docLine__fileInfo} style={{ color: token.colorTextDescription }}>
+                    {Number(size / 1000) > 1000
+                      ? `${(Number(size / 1000) / 1000).toFixed(2)}МБ`
+                      : `${Math.round(Number(size / 1000))}КБ`}
+                  </span>
+                  {date &&
+                    <span className={styles.docLine__fileInfo} style={{ marginLeft: 10 }}>{moment(date).format('DD.MM.YYYY HH.mm')}</span>
+                  }
+                </Flex>
+                {downloading && <Spin />}
+              </Flex>
+            </a>
+            {!downloading && <Tooltip styles={{ body: { fontSize: 12 } }} placement="top" title={"Скачать"} color="#0061aa"> <Button
+              icon={<DownloadOutlined style={{ fontSize: 24 }} />}
+              size="large"
+
+              onClick={async () => {
+                if (!downloading) {
+                  setDownloading(true)
+                  try {
+                    if (!await openDocs(id, name, true, sig)) {
+                      messageApi.open({
+                        type: 'error',
+                        content: 'Ошибка скачивания файла',
+                      });
+                    }
+                  } catch (error) {
+                    console.log(error);
+
+                  }
+                  setDownloading(false)
+                }
+              }}
+              className={styles.iconButton}
+            // style={{ color: token.colorTextDescription }}
+            /></Tooltip>}
+          </Flex>
+          {/* {sig && <SafetyOutlined className={styles.iconButton} style={{ fontSize: 24 }} size="small"
+              onClick={async () => {
+                if (!isChecking) {
+                  setIsChecking(true)
+                  const checked = await checkSig(idDocForCheckSig, id)
+
+                  setIsChecking(false)
+                  if (checked && checked.status === "OK") {
+                    // console.log("checked",JSON.parse(checked.data));
+                    setChekingValue(JSON.parse(checked.data))
+                  } else {
+                    messageApi.open({
+                      type: 'error',
+                      content: 'Ошибка проверки подписи',
+                    });
+                  }
+                }
+                // console.log("id", id);
+                // console.log("idDocForCheckSig", idDocForCheckSig);
+              }}
+            />} */}
+          {sig && <Tooltip styles={{ body: { fontSize: 12 } }} placement="top" title={"Проверить"} color="#0061aa"><Button disabled={isChecking} style={{ fontSize: 14 }} icon={<SafetyOutlined style={{ fontSize: 24 }} />}
+            size="large"
             onClick={async () => {
-              if (!await openDocs(id, sig)) {
+              setIsChecking(true)
+              const checked = await checkSig(idDocForCheckSig, id)
+
+              setIsChecking(false)
+              if (checked && checked.status === "OK") {
+                // console.log("checked",JSON.parse(checked.data));
+                setChekingValue(JSON.parse(checked.data))
+              } else {
                 messageApi.open({
                   type: 'error',
-                  content: 'Ошибка скачивания файла',
+                  content: 'Ошибка проверки подписи',
                 });
               }
+              // console.log("id", id);
+              // console.log("idDocForCheckSig", idDocForCheckSig);
             }}
-          >
-            <Flex align="center" gap={5}>
-
-              <Flex justify="center" align="center">
-                <img src={typeFile[type] ? typeFile[type] : typeFile.noext} alt={`icon ${type}`} style={{ maxWidth: 50 }} />
-              </Flex>
-              <Flex vertical>
-                <span className={styles.docLine__name}>{name}</span>
-                <span className={styles.docLine__fileInfo}>
-                  {Number(size / 1000) > 1000
-                    ? `${(Number(size / 1000) / 1000).toFixed(2)}МБ`
-                    : `${Math.round(Number(size / 1000))}КБ`}
-                </span>
-                {date &&
-                  <span className={styles.docLine__fileInfo} style={{ marginLeft: 10 }}>{moment(date).format('DD.MM.YYYY HH.mm')}</span>
-                }
-              </Flex>
-            </Flex>
-          </a>
-          {sig && <Button disabled={isChecking} style={{ fontSize: 14 }} size="small" onClick={async () => {
-            setIsChecking(true)
-            const checked = await checkSig(idDocForCheckSig, id)
-            
-            setIsChecking(false)
-            if (checked && checked.status==="OK") {
-              // console.log("checked",JSON.parse(checked.data));
-              setChekingValue(JSON.parse(checked.data))
-            } else {
-              messageApi.open({
-                type: 'error',
-                content: 'Ошибка проверки подписи',
-              });
-            }
-            // console.log("id", id);
-            // console.log("idDocForCheckSig", idDocForCheckSig);
-          }}>Проверить подпись</Button>}
+          /></Tooltip>}
 
         </Flex>
-        {signs && <div style={{ marginLeft: 40, position: "relative" }}>
+        {signs && <div style={{ marginLeft: 40, }}>
           {signs.map((item, index) => <FileForDownload sig={true} key={index} type={item.ext} name={item.name} id={item.id} size={item.size} idDocForCheckSig={id} />)}
           {/* <div style={{ position: "absolute", height: "100%", width: 3, borderRadius: 3, backgroundColor:  "lightgray", top: 0, left: -6 }}></div> */}
         </div>}
       </Flex>
-      <Modal
-        open={chekingValue}
-        onCancel={()=>{
-          setChekingValue(false)
-        }}
-      >
-        {chekingValue && chekingValue.signatures?.length>0 &&
-          <Descriptions column={1} style={{ marginTop: 20 }} items={[
-            {
-              key: '1',
-              label: 'Действительность',
-              children: <span style={{ color: chekingValue.isValid ? "green" : "red" }}>{chekingValue.resultText}</span>,
-            },
-            {
-              key: '2',
-              label: 'Дата отчета',
-              children: <span>{chekingValue.reportDate}</span>,
-            },
-            {
-              key: '3',
-              label: 'Издатель сертификата',
-              children: <span >{chekingValue.signatures[0].cert.issuer}</span>,
-            },
-            {
-              key: '4',
-              label: 'Владелец сертификата',
-              children: <span>{chekingValue.signatures[0].cert.subject}</span>,
-            },
-            {
-              key: '5',
-              label: 'Серийный номер',
-              children: <span>{chekingValue.signatures[0].cert.serial}</span>,
-            },
-            {
-              key: '56',
-              label: 'Действует',
-              children: <span>с {moment(chekingValue.signatures[0].cert.notBefore).format('DD.MM.YYYY')} по {moment(chekingValue.signatures[0].cert.notAfter).format('DD.MM.YYYY')}</span>,
-            },
-            {
-              key: '7',
-              label: 'Срок действия ключа подписи',
-              children: <span>с {moment(chekingValue.signatures[0].cert.pkeyNotBefore).format('DD.MM.YYYY')} по {moment(chekingValue.signatures[0].cert.pkeyNotAfter).format('DD.MM.YYYY')}</span>,
-            },
-          ]} />
-        }
-        {chekingValue.resultCode === 1 &&
-          <Typography.Title style={{ color: "red" }} level={5}>Входные данные не являются подписанным сообщением!</Typography.Title>
-        }
-      </Modal>
+      {sig &&
+        <Modal
+          open={chekingValue}
+          onCancel={() => {
+            setChekingValue(false)
+          }}
+        >
+          {chekingValue && chekingValue.signatures?.length > 0 &&
+            <Descriptions column={1} style={{ marginTop: 20 }} items={[
+              {
+                key: '1',
+                label: 'Действительность',
+                children: <span style={{ color: chekingValue.isValid ? "green" : "red" }}>{chekingValue.resultText}</span>,
+              },
+              {
+                key: '2',
+                label: 'Дата отчета',
+                children: <span>{chekingValue.reportDate}</span>,
+              },
+              {
+                key: '3',
+                label: 'Издатель сертификата',
+                children: <span >{chekingValue.signatures[0].cert.issuer}</span>,
+              },
+              {
+                key: '4',
+                label: 'Владелец сертификата',
+                children: <span>{chekingValue.signatures[0].cert.subject}</span>,
+              },
+              {
+                key: '5',
+                label: 'Серийный номер',
+                children: <span>{chekingValue.signatures[0].cert.serial}</span>,
+              },
+              {
+                key: '56',
+                label: 'Действует',
+                children: <span>с {moment(chekingValue.signatures[0].cert.notBefore).format('DD.MM.YYYY')} по {moment(chekingValue.signatures[0].cert.notAfter).format('DD.MM.YYYY')}</span>,
+              },
+              {
+                key: '7',
+                label: 'Срок действия ключа подписи',
+                children: <span>с {moment(chekingValue.signatures[0].cert.pkeyNotBefore).format('DD.MM.YYYY')} по {moment(chekingValue.signatures[0].cert.pkeyNotAfter).format('DD.MM.YYYY')}</span>,
+              },
+            ]} />
+          }
+          {chekingValue.resultCode === 1 &&
+            <Typography.Title style={{ color: "red" }} level={5}>Входные данные не являются подписанным сообщением!</Typography.Title>
+          }
+        </Modal>
+      }
     </>
   );
 }
