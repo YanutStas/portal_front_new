abd375368e16a4e3981030fb3cb804eb6e73792f
Иван Шишкин <luxeivan@gmail.com>
2026-04-30 07:56:02 +0300

    Убрал импорт яндекс карт в MapDisplay

--- src/components/FormComponentsNew/mapComponents/MapDisplay.jsx
+++ src/components/FormComponentsNew/mapComponents/MapDisplay.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import {
-  YMaps,
-  Map,
-  Placemark,
-  Polygon,
-  ZoomControl,
-} from "@pbe/react-yandex-maps";
+// import {
+//   YMaps,
+//   Map,
+//   Placemark,
+//   Polygon,
+//   ZoomControl,
+// } from "@pbe/react-yandex-maps";
 
 const MapDisplay = ({
   mapState,
@@ -16,56 +16,57 @@ const MapDisplay = ({
   onPolygonPointDrag,
 }) => {
   return (
-    <YMaps>
-      <Map
-        state={mapState}
-        width="100%"
-        height="70vh"
-        onClick={onClick}
-        instanceRef={mapRef}
-      >
-        <ZoomControl />
-        {selectedPoint && (
-          <Placemark
-            geometry={selectedPoint}
-            options={{
-              draggable: true,
-              preset: "islands#redCircleDotIcon",
-            }}
-            onDragEnd={(e) => {
-              const newCoords = e.get("target").geometry.getCoordinates();
-              onPolygonPointDrag(newCoords, "point");
-            }}
-          />
-        )}
-        {polygonPoints?.length > 0 && (
-          <>
-            <Polygon
-              geometry={[polygonPoints]}
-              options={{
-                fillColor: "rgba(0, 0, 255, 0.3)",
-                strokeColor: "#0000FF",
-                strokeWidth: 2,
-              }}
-            />
-            {polygonPoints.map((point, index) => (
-              <Placemark
-                key={index}
-                geometry={point}
-                options={{
-                  draggable: true,
-                  preset: "islands#blueCircleDotIcon",
-                }}
-                onDragEnd={(e) => {
-                  const newCoords = e.get("target").geometry.getCoordinates();
-                  onPolygonPointDrag(newCoords, index);
-                }}
-              />
-            ))}
-          </>
-        )}
-      </Map>
-    </YMaps>
+    // <YMaps>
+    //   <Map
+    //     state={mapState}
+    //     width="100%"
+    //     height="70vh"
+    //     onClick={onClick}
+    //     instanceRef={mapRef}
+    //   >
+    //     <ZoomControl />
+    //     {selectedPoint && (
+    //       <Placemark
+    //         geometry={selectedPoint}
+    //         options={{
+    //           draggable: true,
+    //           preset: "islands#redCircleDotIcon",
+    //         }}
+    //         onDragEnd={(e) => {
+    //           const newCoords = e.get("target").geometry.getCoordinates();
+    //           onPolygonPointDrag(newCoords, "point");
+    //         }}
+    //       />
+    //     )}
+    //     {polygonPoints?.length > 0 && (
+    //       <>
+    //         <Polygon
+    //           geometry={[polygonPoints]}
+    //           options={{
+    //             fillColor: "rgba(0, 0, 255, 0.3)",
+    //             strokeColor: "#0000FF",
+    //             strokeWidth: 2,
+    //           }}
+    //         />
+    //         {polygonPoints.map((point, index) => (
+    //           <Placemark
+    //             key={index}
+    //             geometry={point}
+    //             options={{
+    //               draggable: true,
+    //               preset: "islands#blueCircleDotIcon",
+    //             }}
+    //             onDragEnd={(e) => {
+    //               const newCoords = e.get("target").geometry.getCoordinates();
+    //               onPolygonPointDrag(newCoords, index);
+    //             }}
+    //           />
+    //         ))}
+    //       </>
+    //     )}
+    //   </Map>
+    // </YMaps>
+    <></>
   );
 };
 
