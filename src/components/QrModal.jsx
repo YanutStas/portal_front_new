c3435959a9b16896f560409fd28f956347921805
Иван Шишкин <luxeivan@gmail.com>
2026-05-28 16:51:58 +0300

    Сделал точку на карте

--- src/components/QrModal.jsx
+++ src/components/QrModal.jsx
@@ -1,7 +1,9 @@
-import { QRCode, Modal, Typography, Descriptions } from 'antd';
+import { QRCode, Modal, Typography, Descriptions, Tabs } from 'antd';
 import { useEffect, useRef, useState } from 'react';
 
-export const QrModal = ({ open, onCancel, value, title }) => {
+
+
+export const QrModal = ({ open, onCancel, value, title, paymentDetails }) => {
     const containerRef = useRef(null);
     const [qrSize, setQrSize] = useState(500);
 
@@ -28,42 +30,39 @@ export const QrModal = ({ open, onCancel, value, title }) => {
         return () => observer.disconnect();
     }, [open]);
 
+    const items = [
+        {
+            key: '1',
+            label: 'QR код',
+            children: <>
+                <Typography.Text type="secondary">Отсканируйте в банковском приложении</Typography.Text>
+                <div ref={containerRef} style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+                    <QRCode
+                        errorLevel="H"
+                        value={value}
+                        size={qrSize}
+                        type="svg" // SVG масштабируется без потери качества
+                    />
+                </div>
+            </>,
+        },
+        {
+            key: '2',
+            label: 'Реквизиты',
+            children: (paymentDetails && paymentDetails.length > 0) ? <Descriptions size='small' column={1} items={paymentDetails} /> : false,
+        },
+    ];
+
     return (
         <Modal title={title} open={open} onCancel={onCancel} footer={null} width={550}>
-            <Typography.Title level={4}>Реквизиты</Typography.Title>
-            <Descriptions size='small' column={1} items={[
-                {
-                    key:1,
-                    label: "ИНН",
-                    children:"897587963261"
-                },
-                {
-                    key:2,
-                    label: "КПП",
-                    children:"97221435326"
-                },
-                {
-                    key:3,
-                    label: "БИК",
-                    children:"2343457879"
-                },
-                {
-                    key:4,
-                    label: "Наименование банка",
-                    children:"ПАО Сбербанк"
-                },
-            ]}/>
-                    {/* Контейнер растягивается на всю ширину тела модалки */}
-            <Typography.Title level={4}>QR код</Typography.Title>
-            <Typography.Text type="secondary">Отсканируйте в банковском приложении</Typography.Text>
-                    <div ref={containerRef} style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-                        <QRCode
-                            errorLevel="H"
-                            value={value}
-                            size={qrSize}
-                            type="svg" // SVG масштабируется без потери качества
-                        />
-                    </div>            
+            {/* <Typography.Title level={4}>Реквизиты</Typography.Title> */}
+
+            <Tabs defaultActiveKey="1" items={items} />
+
+
+            {/* Контейнер растягивается на всю ширину тела модалки */}
+            {/* <Typography.Title level={4}>QR код</Typography.Title> */}
+
         </Modal>
     );
 };
