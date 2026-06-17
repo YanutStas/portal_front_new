896a220afdf72b92883bbe7d21abcce33e8a73ee
Stanislav <yanut995@gmail.com>
2026-06-17 17:20:17 +0300

    Поменял "Руководство пользователя"

--- src/components/Documentation/UserGuide.jsx
+++ src/components/Documentation/UserGuide.jsx
@@ -7,7 +7,8 @@ import FileIcon from "../FileIcon";
 export default function UserGuide() {
   return (
     <Flex >
-      <FileIcon name="Руководство пользователя портала цифровых услуг АО «Мособлэнерго»" sizeKb={3100} url={"https://www.mosoblenergo.ru/back/uploads/Rukovodstvo_polzovatelya_Portala_Czifrovyh_uslug_2026_138df18462.pdf"} ext={".pdf"} />
+      {/* <FileIcon name="Руководство пользователя портала цифровых услуг АО «Мособлэнерго»" sizeKb={3100} url={"https://www.mosoblenergo.ru/back/uploads/Rukovodstvo_polzovatelya_Portala_Czifrovyh_uslug_2026_138df18462.pdf"} ext={".pdf"} /> */}
+      <FileIcon name="Руководство пользователя портала цифровых услуг АО «Мособлэнерго»" sizeKb={3100} url={"https://www.mosoblenergo.ru/back/uploads/Rukovodstvo_polzovatelya_portala_czifrovyh_uslug_AO_Mosoblenergo_26e756e933.pdf"} ext={".pdf"} />
     </Flex>
   );
 }
